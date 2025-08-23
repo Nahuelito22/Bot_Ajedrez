13e2a45c6784a8476ca5cfc1009eef83ce34e28b
Nahuel <matiasghilardisalinas@gmail.com>
2025-08-23 18:58:47 -0300

    Se agrego una const/api para pruebas y testint local antes del merge al main

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,12 @@ var board = null;
 var game = new Chess();
 var statusEl = document.getElementById('status');
 var pgnEl = document.getElementById('pgn');
-const API_URL = "https://nahuelito22-bot-ajedrez.hf.space/predict_move";
+
+// Api URL del bot de ajedrez
+// const API_URL = "https://nahuelito22-bot-ajedrez.hf.space/predict_move";
+
+// Api URL del bot de ajedrez (local para pruebas)
+const API_URL = "http://127.0.0.1:8000/predict_move";
 
 var isAiThinking = false;
 
