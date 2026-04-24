d321c3f136d203c4b92cc003728b732f9fb3269d
Nahuel <matiasghilardisalinas@gmail.com>
2026-04-24 16:31:51 -0300

    cleanup: remove legacy frontend docs folder

--- docs/config.js
+++
@@ -1,5 +0,0 @@
-
-const config = {
-    // Apuntamos por defecto a Hugging Face para que funcione tanto local como en producción
-    API_URL: 'https://nahuelito22-bot-ajedrez.hf.space/predict_move'
-};
